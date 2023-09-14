41a5e96507dcf993a038b6816d8f264fcf8b9c64
AndreyK1993 <andrey.kurilenko27@gmail.com>
2023-09-14 22:56:58 +0300

    Update 1

--- src/multitask/Main.java
+++ src/multitask/Main.java
@@ -1,4 +1,44 @@
 package multitask;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        // Оголошення та створення масиву.
+        // 4 рядки, 4 стовпця
+        int[][] nums4 = {
+                { 5, 16, 23, 56 },
+                { 3, 27, 52, 84 },
+                { 76, 43, 69, 92 },
+                { 8, 11, 33, 55 },
+        };
+        int count = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter searching int value: ");
+        int search = scanner.nextInt();
+
+        // Пошук значення.
+        // Перебір за допомогою циклу for-each
+        for (int[] nums3 : nums4) {
+            for (int nums2 : nums3) {
+                for (int nums1 : nums2) {
+                    for (int value : nums1) {
+                        if (value == search) {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (count > 0)
+            System.out.println(count + " match(es) of value " +
+                    search + " has found.");
+        else System.out.println("No match(es) of value " +
+                search + " has found.");
+
+    }
 }
